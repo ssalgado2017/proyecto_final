045a36f0e3a29ea16e90240bf99097ba3bf08b8f
Sandra <sandra.salgado@bci.cl>
2018-05-09 12:44:48 -0300

    scaffold Clients

---
+++ test/controllers/clients_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class ClientsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @client = clients(:one)
+  end
+
+  test "should get index" do
+    get clients_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_client_url
+    assert_response :success
+  end
+
+  test "should create client" do
+    assert_difference('Client.count') do
+      post clients_url, params: { client: { dv: @client.dv, institution_id: @client.institution_id, last_name: @client.last_name, name: @client.name, rut: @client.rut } }
+    end
+
+    assert_redirected_to client_url(Client.last)
+  end
+
+  test "should show client" do
+    get client_url(@client)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_client_url(@client)
+    assert_response :success
+  end
+
+  test "should update client" do
+    patch client_url(@client), params: { client: { dv: @client.dv, institution_id: @client.institution_id, last_name: @client.last_name, name: @client.name, rut: @client.rut } }
+    assert_redirected_to client_url(@client)
+  end
+
+  test "should destroy client" do
+    assert_difference('Client.count', -1) do
+      delete client_url(@client)
+    end
+
+    assert_redirected_to clients_url
+  end
+end
