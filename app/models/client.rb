db940e92eb1f8771895f70f396ff7919b6cc5403
Sandra <sandra.salgado@bci.cl>
2018-08-21 13:52:07 -0300

    formateo de vista de cotización y clientes

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,10 @@ class Client < ApplicationRecord
   belongs_to :institution
   has_many :user_clients, dependent: :destroy
   has_many :users,through: :user_clients, dependent: :destroy
+
+
+  def fullname
+    "#{name} #{last_name}"
+  end
+
 end
