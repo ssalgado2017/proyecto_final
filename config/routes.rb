5b5d121ed3d45cf0cf5faaa88b8bb6c1e589bd1f
Sandra <sandra.salgado@bci.cl>
2018-08-27 15:58:01 -0300

    cambios menores

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         registrations: 'users/registrations'
       }
-  root 'clients#index'
+  root 'quotations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
